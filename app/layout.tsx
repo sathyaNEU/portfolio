95e9af55301a2bc43f90b6a1402e37bbeaa8fdef
sathayNEU <balachandar.sa@northeastern.edu>
2025-12-31 22:47:12 -0500

    fix: basepath

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type React from "react"
 import type { Metadata } from "next"
 import { Geist_Mono } from "next/font/google"
 import { Analytics } from "@vercel/analytics/next"
-import "./globals.css"
+import "../styles/globals.css"
 
 const geistMono = Geist_Mono({ subsets: ["latin"] })
 
